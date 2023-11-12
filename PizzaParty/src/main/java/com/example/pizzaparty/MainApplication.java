70f57d9ce4363ba72eb1f2b9245e5de9a855c059
anikat2 <anikathakur212@gmail.com>
2023-11-11 23:36:04 -0500

    Added basic view switch
    Added basic view switch

--- PizzaParty/src/main/java/com/example/pizzaparty/MainApplication.java
+++ PizzaParty/src/main/java/com/example/pizzaparty/MainApplication.java
@@ -1,17 +1,20 @@
 package com.example.pizzaparty;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MainApplication extends javafx.application.Application {
+
+    private Button specialtyButton;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ PizzaParty/src/main/java/com/example/pizzaparty/SpecialtiesController.java
@@ -0,0 +1,36 @@
+package com.example.pizzaparty;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SpecialtiesController {
+    private MainMenuController mainMenuController;
+    public void backToMainAction(ActionEvent actionEvent) {
+        Stage mainStage = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+        AnchorPane root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("main-menu.fxml"));
+            root = (AnchorPane) loader.load();
+            Scene scene = new Scene(root, 500, 400);
+            mainStage.setScene(scene);
+            mainStage.show();
+        }catch (IOException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("ERROR");
+            alert.setHeaderText("Loading View1.fxml.");
+            alert.setContentText("Couldn't load View1.fxml.");
+            alert.showAndWait();
+        }
+    }
+
+    public void setMainController (MainMenuController controller){
+        mainMenuController = controller;
+    }
+}
